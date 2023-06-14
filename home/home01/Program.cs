5c4805df2e49a7fc933bca8e9b453348311ff7ce
sigiriyz <dima170490@mail.ru>
2023-06-15 00:16:23 +1000

    home

--- home/home01/Program.cs
+++ home/home01/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Console.Write("input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine("Square is: " + result);
+
+
+Console.WriteLine("input first: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input second: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA == numberB * numberB)
+{
+    Console.WriteLine("Ferst number = square second");
+}
+else
+{
+    Console.WriteLine("Ferst number != square second");
+}
+
+
+Console.WriteLine("input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number * (-1);
+while(count <= number)
+{
+    Console.Write(count + ", ");
+    count = count + 1; //count +=1 count++
+}
+*/
+
+
+
+
+/*
+Console.WriteLine("input number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.Write("first > second");
+}
+else
+{
+    Console.Write("secondfirst > first");
+}
+
+
+Console.WriteLine("input number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+Console.Write("max number: " + max);
+
+
+Console.WriteLine("input number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if(numberA % 2 == 0)
+{
+    Console.WriteLine("even number: " + numberA);
+}
+else
+{
+    Console.WriteLine("try again: ");
+}
+*/
+
