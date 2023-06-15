c4c8316ce09f82ca9880f70c84840171befec62d
sigiriyz <dima170490@mail.ru>
2023-06-15 18:43:52 +1000

    home

--- home/home01/Program.cs
+++ home/home01/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 Console.WriteLine("input number: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("input number: ");
@@ -12,7 +12,7 @@ else
     Console.Write(numberB + " > " + numberA);
 }
 
-
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 Console.WriteLine("input number: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("input number: ");
@@ -24,7 +24,7 @@ if(numberB > max) max = numberB;
 if(numberC > max) max = numberC;
 Console.Write("max number: " + max);
 
-
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 Console.WriteLine("input number: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 if(numberA % 2 == 0)
@@ -36,7 +36,7 @@ else
     Console.WriteLine("try again: ");
 }
 
-
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 Console.WriteLine("input number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 2;
